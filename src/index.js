77b925d3abe1cbf8a7b5acb5217cbd378289f9fd
ShengzhiLuo <sl5009@cumc.columbia.edu>
2022-07-28 09:55:08 -0400

    readme

--- src/index.js
+++ src/index.js
@@ -51,8 +51,8 @@ window.addEventListener('load', () => {
         constructor(gamewidth, gameheight) {
             this.gameWidth = gamewidth;
             this.gameHeight = gameheight;
-            this.run_image = document.getElementById("playerImage_run");
-            // this.jump_image = document.getElementById("playerImage_jump");
+            // this.run_image = document.getElementById("playerImage_run");
+            this.jump_image = document.getElementById("playerImage_jump");
             this.width = 100;
             this.height = 100;
             this.x = 0;
@@ -75,7 +75,7 @@ window.addEventListener('load', () => {
 
         }
         draw(context) {
-            context.drawImage(this.run_image, this.frameX * this.width, this.frameY * this.height, this.width,this.height, this.x, this.y, this.width, this.height);
+            context.drawImage(this.jump_image, this.frameX * this.width, this.frameY * this.height, this.width,this.height, this.x, this.y, this.width, this.height);
         }  
         update(input, deltaTime, platforms) {
             // debugger;
@@ -93,16 +93,18 @@ window.addEventListener('load', () => {
             
             // collision detection
             if (input.keys.indexOf("ArrowUp") !== -1){   
-                this.vy = -22;               
+                this.vy = -22;
+                // if (this.frameTimer > this.frameInterval) {                        
+                //     if (this.frameX >= this.maxFrameY) this.frameX = 0;
+                //         else this.frameX++;
+                //         this.frameTimer = 0;
+                //         } else {
+                //             this.frameTimer += deltaTime;
+                //         }            
+                if (this.frameX >= this.maxFrameY) this.frameX = 0;
+                else this.frameX++;
             } else if (input.keys.indexOf("ArrowRight") !== -1) {               
                 this.speed = 5;               
-                if (this.frameTimer > this.frameInterval) {                        
-                    if (this.frameX >= this.maxFrameX) this.frameX = 0;                           
-                    else this.frameX++;                    
-                    this.frameTimer = 0;                   
-                } else {                   
-                    this.frameTimer += deltaTime;                    
-                }               
             } else {               
                 this.speed = 0;               
             }           
@@ -123,6 +125,9 @@ window.addEventListener('load', () => {
             if (this.y > this.gameHeight - this.height) {            
                 this.y = this.gameHeight - this.height;         
             } 
+            if (this.y < 0) {            
+                this.y = this.height;         
+            }
             
             
         }
